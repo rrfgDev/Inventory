bf816ab1d886d16aad83c8861f26becc274c612d
Ryan Gouvea <119331903+rrfgDev@users.noreply.github.com>
2023-07-02 07:16:38 -0300

    Criacao do Header e Footer
    Criamos o Header.php e o Foooter.php juntamente com as documentacoes.

---
+++ Master/includes/footer.php
@@ -0,0 +1,73 @@
+
+  <footer class="bg-light text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Social media -->
+    
+	<section class="mb-4">
+    
+	<!-- Facebook -->
+      <a
+        class="btn btn-primary btn-floating m-1"
+		style="background-color: transparent; border: none;"
+        href="#!"
+        role="button"
+        ><img src="./img/icons/facebook.png"height="20">
+      ></a>
+
+      <!-- Twitter -->
+      <a
+        class="btn btn-primary btn-floating m-1"
+		style="background-color: transparent; border: none"
+        href="#!"
+        role="button"
+        > <img src="./img/icons/twitter.png"height="20">
+      ></a>
+
+      <!-- Google -->
+      <a
+        class="btn btn-primary btn-floating m-1"
+		style="background-color: transparent; border: none"
+        href="https://moodle.sistemadivinaprovidencia.org/"
+        role="button"
+        ><img src="./img/icons/cromada.png"height="20">
+      ></a>
+
+      <!-- Instagram -->
+      <a
+        class="btn btn-primary btn-floating m-1"
+		style="background-color: transparent; border: none"
+        href="#!"
+        role="button"
+        ><img src="./img/icons/instagram.png"height="20">
+      ></a>
+
+      <!-- Linkedin -->
+      <a
+        class="btn btn-primary btn-floating m-1"
+		style="background-color: transparent; border: none"
+        href="#!"
+        role="button"
+        > <img src="./img/icons/linkedin.png"height="20">
+      ></a>
+      <!-- Github -->
+      <a
+        class="btn btn-primary btn-floating m-1"
+		style="background-color: transparent; border: none"
+        href="#!"
+        role="button"
+        > <img src="./img/icons/github.png"height="20">
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2); border-width :1px;  font-size: 0.8em;">
+  © 2020 Copyright Desenvolvido por:
+  <a class="text-white" href="#">Ryan Gouvêa</a>
+</div>
+
+  <!-- Copyright -->
+</footer>
